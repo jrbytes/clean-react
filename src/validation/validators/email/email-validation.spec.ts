b0919f85eeb4be4d5e0b73f2fdd22398dd7ecc1e
Jr Bytes <itamazor@gmail.com>
2022-06-05 21:15:00 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,18 @@
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('')
-    expect(error).toEqual(new InvalidFieldError('email'))
+    const randomWord = faker.random.word()
+    const sut = new EmailValidation(randomWord)
+    const error = sut.validate(faker.random.word())
+    expect(error).toEqual(new InvalidFieldError(randomWord))
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.random.word())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
