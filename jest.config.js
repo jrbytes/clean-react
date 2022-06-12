c424d10bd73b8c338a4e247bb6c206245e549f38
Jr Bytes <itamazor@gmail.com>
2022-06-12 10:38:21 -0300

    chore: change jest to ignore index with imports files

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/components/router/**/*',
-    '!<rootDir>/src/domain/models/**/*',
-    '!<rootDir>/src/domain/usecases/**/*',
+    '!<rootDir>/src/**/index.ts',
     '!**/*.d.ts'
   ],
   coverageDirectory: 'coverage',
