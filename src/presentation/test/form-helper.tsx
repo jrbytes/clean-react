612006cd7bfeda570b8cca6c0644e3526f3dffc3
Jr Bytes <itamazor@gmail.com>
2023-04-03 14:36:31 -0300

    refactor: improve input layout

--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -2,33 +2,61 @@ import { RenderResult } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import faker from 'faker'
 
-export const testChildCount = (sut: RenderResult, fieldName: string, count: number): void => {
+export const testChildCount = (
+  sut: RenderResult,
+  fieldName: string,
+  count: number
+): void => {
   const element = sut.getByTestId(fieldName)
   expect(element.childElementCount).toBe(count)
 }
 
-export const testButtonIsDisabled = (sut: RenderResult, fieldName: string, isDisabled: boolean): void => {
+export const testButtonIsDisabled = (
+  sut: RenderResult,
+  fieldName: string,
+  isDisabled: boolean
+): void => {
   const button = sut.getByTestId(fieldName) as HTMLButtonElement
   expect(button.disabled).toBe(isDisabled)
 }
 
-export const testStatusForField = (sut: RenderResult, fieldName: string, validationError?: string): void => {
-  const fieldStatus = sut.getByTestId(`${fieldName}-status`)
-  expect(fieldStatus.title).toBe(validationError || 'Tudo certo!')
-  expect(fieldStatus.textContent).toBe(validationError ? '🔴' : '🟢')
+export const testStatusForField = (
+  sut: RenderResult,
+  fieldName: string,
+  validationError: string = ''
+): void => {
+  const wrap = sut.getByTestId(`${fieldName}-wrap`)
+  const field = sut.getByTestId(fieldName)
+  const label = sut.getByTestId(`${fieldName}-label`)
+  expect(wrap.getAttribute('data-status')).toBe(
+    validationError ? 'invalid' : 'valid'
+  )
+  expect(field.title).toBe(validationError)
+  expect(label.title).toBe(validationError)
 }
 
-export const populateField = async (sut: RenderResult, fieldName: string, value = faker.random.word()): Promise<void> => {
+export const populateField = async (
+  sut: RenderResult,
+  fieldName: string,
+  value = faker.random.word()
+): Promise<void> => {
   const input = sut.getByTestId(fieldName)
   await userEvent.type(input, value)
 }
 
-export const testElementExists = (sut: RenderResult, fieldName: string): void => {
+export const testElementExists = (
+  sut: RenderResult,
+  fieldName: string
+): void => {
   const element = sut.getByTestId(fieldName)
   expect(element).toBeTruthy()
 }
 
-export const testElementText = (sut: RenderResult, fieldName: string, text: string): void => {
+export const testElementText = (
+  sut: RenderResult,
+  fieldName: string,
+  text: string
+): void => {
   const element = sut.getByTestId(fieldName)
   expect(element.textContent).toBe(text)
 }
