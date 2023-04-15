fff3d07ee5f6c84bc82dcf5ca1b9741f580ef321
Jr Bytes <itamazor@gmail.com>
2023-04-15 18:37:24 -0300

    test: ensure SurveyItem load icon and day correctly

--- src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.spec.tsx
@@ -7,16 +7,29 @@ import { IconName } from '@/presentation/components'
 
 describe('SurveyItem Component', () => {
   test('Should render with correct values', () => {
-    const survey = mockSurveyModel()
-    survey.didAnswer = true
-    survey.date = new Date('2023-01-10T00:00:00')
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: true,
+      date: new Date('2023-01-03T00:00:00'),
+    })
     render(<SurveyItem survey={survey} />)
     expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbUp)
     expect(
       screen.getByText(survey.question, { selector: 'p' })
     ).toBeInTheDocument()
-    expect(screen.getByLabelText('day').textContent).toBe('10')
+    expect(screen.getByLabelText('day').textContent).toBe('03')
     expect(screen.getByLabelText('month').textContent).toBe('jan')
     expect(screen.getByLabelText('year').textContent).toBe('2023')
+  })
+
+  test('Should render with correct values', () => {
+    const survey = Object.assign(mockSurveyModel(), {
+      didAnswer: false,
+      date: new Date('2019-05-23T00:00:00'),
+    })
+    render(<SurveyItem survey={survey} />)
+    expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbDown)
+    expect(screen.getByLabelText('day').textContent).toBe('23')
+    expect(screen.getByLabelText('month').textContent).toBe('mai')
+    expect(screen.getByLabelText('year').textContent).toBe('2019')
   })
 })
