462133f29901f69de1c798822b7febdaa7d0446f
Jr Bytes <itamazor@gmail.com>
2023-04-18 15:07:44 -0300

    test: ensure LocalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -21,6 +21,13 @@ describe('LocalStorageAdapter', () => {
     )
   })
 
+  test('Should call localStorage.removeItem with value is null', () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    sut.set(key, undefined)
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
+  })
+
   test('Should call localStorage.getItem with correct value', () => {
     const sut = makeSut()
     const key = faker.database.column()
