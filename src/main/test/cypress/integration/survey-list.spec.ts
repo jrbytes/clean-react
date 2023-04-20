d04da9b58808d9a76104483e765fba8e224190cd
Jr Bytes <itamazor@gmail.com>
2023-04-20 15:30:46 -0300

    test: ensure SurveyList reload on button click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -8,6 +8,9 @@ const mockUnexpectedError = (): void => {
 const mockAccessDeniedError = (): void => {
   Http.mockForbiddenError(path, 'GET')
 }
+const mockSuccess = (): void => {
+  Http.mockOk(path, 'GET', 'fx:survey-list')
+}
 
 describe('login', () => {
   beforeEach(() => {
@@ -28,6 +31,24 @@ describe('login', () => {
       .should('contain.text', 'Tentar novamente')
   })
 
+  it('should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.get('div')
+      .should(
+        'contain.text',
+        'Algo de errado aconteceu. Tente novamente em breve.'
+      )
+      .find('button')
+      .should('contain.text', 'Tentar novamente')
+    mockSuccess()
+    cy.get('div')
+      .find('button')
+      .should('contain.text', 'Tentar novamente')
+      .click()
+    cy.get('li:not(:empty)').should('have.length', 2)
+  })
+
   it('should logout on AccessDeniedError', () => {
     mockAccessDeniedError()
     cy.visit('')
@@ -46,5 +67,30 @@ describe('login', () => {
     cy.visit('')
     cy.get('header').find('a').should('contain.text', 'Sair').click()
     Helper.testUrl('/login')
+  })
+
+  it('should present survey items', () => {
+    mockSuccess()
+    cy.visit('')
+    cy.get('li:empty').should('have.length', 3)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.equal(li.find('[aria-label=day]').text(), '03')
+      assert.equal(li.find('[aria-label=month]').text(), 'fev')
+      assert.equal(li.find('[aria-label=year]').text(), '2018')
+      assert.equal(li.find('p').text(), 'Question 1')
+      cy.fixture('icons').then((icon) => {
+        assert.equal(li.find('img').attr('src'), icon.thumbUp)
+      })
+    })
+    cy.get('li:nth-child(2)').then((li) => {
+      assert.equal(li.find('[aria-label=day]').text(), '20')
+      assert.equal(li.find('[aria-label=month]').text(), 'out')
+      assert.equal(li.find('[aria-label=year]').text(), '2020')
+      assert.equal(li.find('p').text(), 'Question 2')
+      cy.fixture('icons').then((icon) => {
+        assert.equal(li.find('img').attr('src'), icon.thumbDown)
+      })
+    })
   })
 })
