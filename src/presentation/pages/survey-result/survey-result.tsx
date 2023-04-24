cc0a60ea36d1cda6d352a3f4bf44719ea90e7dbd
Jr Bytes <itamazor@gmail.com>
2023-04-24 17:49:50 -0300

    feat: add Calendar to SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import FlipMove from 'react-flip-move'
 
-import Styles from './survey-result-styles.scss'
-import { Footer, Header, Loading } from '@/presentation/components'
+import Styles from './styles.scss'
+import { Calendar, Footer, Header, Loading } from '@/presentation/components'
 
 const SurveyResult: React.FC = () => {
   return (
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <h2>Qual sua linguagem de programação favorita?</h2>
+        <hgroup>
+          <Calendar date={new Date()} className={Styles.calendarWrap} />
+          <h2>
+            Qual sua linguagem de programação favorita? Qual sua linguagem de
+            programação favorita? Qual sua linguagem de programação favorita?
+          </h2>
+        </hgroup>
         <FlipMove className={Styles.answersList}>
           <li>
             <img src="https://legacy.reactjs.org/logo-og.png" alt="" />
