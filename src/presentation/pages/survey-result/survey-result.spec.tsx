8ff97479d29d6cacf2b1d81480c571f7f16d2950
Jr Bytes <itamazor@gmail.com>
2023-04-27 12:32:57 -0300

    test: ensure SurveyResult calls LoadSurveyResult

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 import { SurveyResult } from '@/presentation/pages'
 import { UnexpectedError } from '@/domain/errors'
 import { ApiContext } from '@/presentation/contexts'
-import { mockAccountModel } from '@/domain/test'
+import { LoadSurveyResultSpy, mockAccountModel } from '@/domain/test'
 
-const makeSut = (): void => {
+type SutTypes = {
+  loadSurveyResultSpy: LoadSurveyResultSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyResultSpy = new LoadSurveyResultSpy()
   render(
     <ApiContext.Provider
       value={{
@@ -14,9 +19,12 @@ const makeSut = (): void => {
         getCurrentAccount: () => mockAccountModel(),
       }}
     >
-      <SurveyResult />
+      <SurveyResult loadSurveyResult={loadSurveyResultSpy} />
     </ApiContext.Provider>
   )
+  return {
+    loadSurveyResultSpy,
+  }
 }
 
 describe('SurveyResult Component', () => {
@@ -27,5 +35,12 @@ describe('SurveyResult Component', () => {
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByText(error.message)).not.toBeInTheDocument()
     expect(screen.queryByText('Aguarde...')).not.toBeInTheDocument()
+    await waitFor(() => surveyResult)
+  })
+
+  test('Should call LoadSurveyResult', async () => {
+    const { loadSurveyResultSpy } = makeSut()
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(loadSurveyResultSpy.callsCount).toBe(1)
   })
 })
