4dacd226393d6074ffc2f9efd67c74a03816a124
Jr Bytes <itamazor@gmail.com>
2023-04-29 16:26:42 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/survey-list/components/list/list.spec.tsx
+++ src/presentation/pages/survey-list/components/list/list.spec.tsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
 
 import { mockSurveyModel } from '@/domain/test'
 import { Item } from '@/presentation/pages/survey-list/components'
 import { IconName } from '@/presentation/components'
+import { LoadSurveyList } from '@/domain/usecases'
+
+type SutTypes = {
+  survey: LoadSurveyList.Model
+}
+
+const makeSut = (survey = mockSurveyModel()): SutTypes => {
+  render(
+    <Router history={createMemoryHistory()}>
+      <Item survey={survey} />
+    </Router>
+  )
+  return {
+    survey,
+  }
+}
 
 describe('Item Component', () => {
   test('Should render with correct values', () => {
-    const survey = Object.assign(mockSurveyModel(), {
+    const surveyMock = Object.assign(mockSurveyModel(), {
       didAnswer: true,
       date: new Date('2023-01-03T00:00:00'),
     })
-    render(<Item survey={survey} />)
+    const { survey } = makeSut(surveyMock)
     expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbUp)
     expect(
       screen.getByText(survey.question, { selector: 'p' })
@@ -22,11 +40,11 @@ describe('Item Component', () => {
   })
 
   test('Should render with correct values', () => {
-    const survey = Object.assign(mockSurveyModel(), {
+    const surveyMock = Object.assign(mockSurveyModel(), {
       didAnswer: false,
       date: new Date('2019-05-23T00:00:00'),
     })
-    render(<Item survey={survey} />)
+    makeSut(surveyMock)
     expect(screen.getByRole('img')).toHaveProperty('src', IconName.thumbDown)
     expect(screen.getByLabelText('day').textContent).toBe('23')
     expect(screen.getByLabelText('month').textContent).toBe('mai')
