36931e3a38ad5c7a8ca74a84702e2790e10d100c
Jr Bytes <itamazor@gmail.com>
2023-04-29 11:30:17 -0300

    test: ensure SurveyResult reloads on reload button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -5,6 +5,9 @@ const path = /surveys/
 const mockUnexpectedError = (): void => {
   Http.mockServerError(path, 'GET')
 }
+const mockSuccess = (): void => {
+  Http.mockOk(path, 'GET', 'fx:survey-result')
+}
 
 describe('SurveyResult', () => {
   beforeEach(() => {
@@ -23,5 +26,23 @@ describe('SurveyResult', () => {
       )
       .find('button')
       .should('contain.text', 'Tentar novamente')
+  })
+
+  it('should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.get('div')
+      .should(
+        'contain.text',
+        'Algo de errado aconteceu. Tente novamente em breve.'
+      )
+      .find('button')
+      .should('contain.text', 'Tentar novamente')
+    mockSuccess()
+    cy.get('div')
+      .find('button')
+      .should('contain.text', 'Tentar novamente')
+      .click()
+    cy.get('header').should('exist')
   })
 })
